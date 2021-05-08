dbe954cfa469f47e3fbc14c92db907a64b00c8e1
MohamedAgroudy <76107395+MohamedAgroudy@users.noreply.github.com>
2021-05-08 13:33:44 +0200

    Add files via upload

---
+++ signin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="layout/css/master.css">
+    <link rel="stylesheet" href="layout/css/style.css">
+    <title>Sign In</title>
+</head>
+<body>
+<?php include('includes/header.php'); ?>
+
+    <div class="flex align-center justify-center h-100">
+        <div class="box-wrapper flex relative">
+            <div class="or-sep"></div>
+            <div class="item left">
+                <h1>Sign In</h1>
+                <form action="" method="post">
+                    <input type="text" name="phone" placeholder="Phone Number"><br>
+                    <input type="password" name="password" placeholder="password"><br>
+                    <input type="submit" value="Sign Up">
+                </form>
+            </div>
+            <div class="item right">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
