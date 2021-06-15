6a2290a848fd7cd9ece8cd496baaa34a51294b08
amr hegazi <amr1808050@miuegypt.edu.eg>
2021-06-15 04:22:52 +0200

    MVC Doc

---
+++ CateringApp/app/models/edituserprofModel.php
@@ -0,0 +1,5 @@
+<?php
+class edituserprofModel extends model{
+     public $title = 'MIU SE305 Blog V1.0';
+     public $subtitle = 'Example of MVC PHP framework';
+}
---
+++ CateringApp/app/models/servicesModel.php
@@ -0,0 +1,5 @@
+<?php
+class servicesModel extends model{
+     public $title = 'MIU SE305 Blog V1.0';
+     public $subtitle = 'Example of MVC PHP framework';
+}
