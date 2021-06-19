1ea5a9e0bad5ee622795044c010c2da589841d26
ahmedhesham-1 <69846692+ahmedhesham-1@users.noreply.github.com>
2021-06-19 20:11:07 +0200

    Update editAdminprofile.php

--- CateringApp/app/views/admin/editAdminprofile.php
+++ CateringApp/app/views/admin/editAdminprofile.php
@@ -1,7 +1,24 @@
- <!DOCTYPE html>
-<html lang="en">
-<head>
+ <?php
+class editAdminprofile extends view{
+  public function output(){
+    $title = $this->model->title;
+    $subtitle = $this->model->subtitle;
+    require APPROOT . '/views/inc/header.php';
+ ?>
 
+
+<div>
+<input type="file"   id="yellow_button" name="uploadfile"  value="upload photo"/>
+
+</div>
+
+<div>
+
+<input type="submit"  id="blue_button" name="pho" value="Update"/>
+</div>
+    
+ 
+<head>
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,33 +26,53 @@
     <link rel="stylesheet" href="../layout/css/style.css">
 </head>
 <body>
-<?php include('../includes/AdminHeader.php'); ?>
+
 <div>
 </div>
+ 
 
+</div>
+
+
+
+
+<?php
+            foreach($this->model->readProfile() as $item){
+            
+            ?>
 <div>
 <button type="button" id="blue_button1">Edit </button>
 </div>
 
 
 <div>
-<u id="Hello"> Hello Admin <u>
+<u id="Hello"> Hello <?php echo  $item->name; ?><u>
 </div>
 
 
 <div>
-<b id="Name"> Name <b>
+<b id="Name"> <?php echo  $item->name; ?> <b>
 </div>
 
 <div>
-<b id="Password"> Password <b>
+<b id="Phone"> <?php echo  $item->phone; ?> <b>
+</div>
+
+<div>
+<b id="Password"> <?php echo  $item->password; ?> <b>
 </div>
 
 
 <span class="circle">
-<img src="../images/party.jpg" alt="Avatar" style="width:180px">
+<img src=<?php echo URLROOT . $item->profilePic; ?> alt="Avatar" style="width:180px">
 </span>
 
+<?php
+             }  
+             ?>
 
-</body>
-</html>
+<?php
+  require APPROOT . '/views/inc/footer.php';
+  }
+}
+?>
